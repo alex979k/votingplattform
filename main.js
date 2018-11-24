5e69818c26ba8cb93f56d185b6791a6d3bf95220
Albert <me@albert.sh>
2018-11-24 23:42:12 +0100

    Add neon-js logic and finish functions

--- main.js
+++ main.js
@@ -1,20 +1,79 @@
 # Set up contract()
 
+import Neon, {api} from "@cityofzion/neon-js"
+import { rpc, api, sc, u } from '@cityofzion/neon-js';
 
+//const neon_js = require('@cityofzion/neon-js');
+//const Neon = neon_js.default;
+
+    contract = 'dea7b1d8b4ccea3bc6d32aa45b583f418274fb5f'; //TODO:CHANGE
+    networkUrl = 'http://localhost:30333';
+    neoscanUrl = 'http://localhost:4000/api/main_net';
+
+    //constructor() {
+        // this.getDomainAddress(this.contract, 'asasd').then(res => alert(res), err => console.log(err));
+        //this.alternateRegister('test5');
+    //}
+
+    function contractInvoke(operation, arg1) {
+
+        const account = Neon.create.account('KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr'); //TODO:CHANGE
+        const args = [arg1];
+        const script = Neon.create.script({
+            scriptHash: this.contract,
+            operation: operation,
+            args
+        });
+
+        const request = {
+            net: this.neoscanUrl,
+            url: this.networkUrl,
+            script,
+            address: account.address,
+            privateKey: account.privateKey,
+            publicKey: account.publicKey,
+            gas: 1,
+            balance: null
+        };
+
+        Neon.doInvoke(request).then(res => console.log(res.response));
+    }
+
+    function contractCall(operation) {
+	scHash="";
+        /*const sb = Neon.create.scriptBuilder();
+        console.log(u.str2hexstring(domain));
+        sb.emitAppCall(this.contract, 'query', [u.str2hexstring(domain)]);
+
+        // Test the script with invokescript
+        rpc.Query
+            .invokeScript(sb.str)
+            .execute(`http://neo-privnet:30333`)
+            .then(data => {
+                alert(this.parserService.ParseStack(data.result.stack));
+            })
+            .catch(err => {
+                console.log(err);
+            });*/
+        return rpc.Query.invokeFunction(scHash, operation, []) //3.9.1
+            .execute(this.neoscanUrl);
+    }
+
+const NUM_VALIDATORS=1;
 const MAX_RAND=10000
 
-function vote(v){
+function async vote(v){
 	randomness=Math.floor(Math.random()*MAX_RAND);
-	publicKey=getElectionPublicKey();
-	contract("vote", homoEncrypt(publicKey, v, randomness));
+	publicKey= await getElectionPublicKey();
+	contractInvoke("vote", homoEncrypt(publicKey, v, randomness));
 }
 
 function homoEncrypt(publicKey, payload, randomness){ //Fake
-
+	return JSON.stringify({pKey: publicKey, value: payload, decrypted: 0, rand: randomness})
 }
 
 function getElectionPublicKey(){
-	return contract("getElectionPublicKey")
+	return contractCall("getElectionPublicKey")
 }
 
 function reverseGraph(graph){
@@ -32,16 +91,16 @@ function reverseGraph(graph){
 }
 		
 
-function getElectionResults(){
-	votes=getAllCyphertexts()
-	web=getWebOfTust()
-	cKeys=getCitizenKeys()
-	//filter votes
-	web=filter
+function async getElectionResults(){
+	votes= await getAllCyphertexts()
+	//web=getWebOfTust()
+	cKeys= await getCitizenKeys()
+	//filter votes - TODO: Improve by analysing the web of trust for weird patterns that indicate manipulation
+	votes=votes.filter((v)=>v.voter in cKeys)
 	//compute result
 	encryptedResult=homomorphicSum(votes.map((v)=>v.cypher))
-	result=getDecryptedResult()
-	publicKey=getElectionPublicKey();
+	result=await getDecryptedResult()
+	publicKey=await getElectionPublicKey();
 	if(!checkEncryption(encryptedResult, result, publicKey)){
 		throw failure;
 	}
@@ -49,26 +108,38 @@ function getElectionResults(){
 }
 
 function homomorphicSum(cyphertexts){ //Fake
-
+	sum=0
+	pKey=cyphertexts[0].pKey
+	for(c in cyphertexts){
+		if(c.pKey==pKey){
+			sum+=c.value;
+		}
+	}
+	return {pKey: pKey, value: sum, decrypted:0}
 }
 
 function checkEncryption(encrypted, decrypted, publicKey){ //Fake
-
+	if(publicKey==encrypted.pKey &&
+		publicKey==decrypted.pKey &&
+		encrypted.value==decrypted.value &&
+		decrypted.decrypted>NUM_VALIDATORS)
+		return true
+	return false
 }
 
 function getDecryptedResult(){
-	return contract("getResult")
+	return contractCall("getResult")
 }
 
 function getAllCyphertexts(){ //[{cypher, voter}, ...]
-	return JSON.parse(contract("getCyphertexts"))
+	return JSON.parse(contractCall("getCyphertexts"))
 }
 
 function getWebOfTust(){
-	return JSON.parse(contract("getWeb"))
+	return JSON.parse(contractCall("getWeb"))
 }
 
-function async getCitizenKeys(){ //retrieve IPFS
-	return await fetch("https://ipfs.io/ipfs/Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a")
-	.then((res)=>res.text())
+function getCitizenKeys(){ //retrieve from IPFS
+	return fetch("https://ipfs.io/ipfs/Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a")
+	.then((res)=>res.json())
 }
